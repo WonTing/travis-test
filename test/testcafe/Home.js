73e8908819fad653cd50fa0fc9f1cedb9a053e90
WonTing <48178778+WonTing@users.noreply.github.com>
2020-07-23 10:49:25 +0800

    fix
    fix

--- test/testcafe/Home.js
+++ test/testcafe/Home.js
@@ -6,5 +6,5 @@ fixture `Home`
 test(`Home 測試`, async t => {
   await t
     .expect(Selector('[data-cafe="testcafe-h1"]').exists).ok()
-    .expect(Selector('[data-cafe="testcafe-h1"]').innerText).eql('HELLO CI/CD')
+    .expect(Selector('[data-cafe="testcafe-h1"]').innerText).eql('HELLO Home')
 })
