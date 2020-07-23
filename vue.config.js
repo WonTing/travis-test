ddded22ab8a521ba70377cb1427849563849d91a
WonTing <48178778+WonTing@users.noreply.github.com>
2020-07-23 11:43:56 +0800

    update vue.config
    update vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/travis-test/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/travis-test-demo/' : '/',
 	pwa: {
 		workboxPluginMode: 'InjectManifest',
     workboxOptions: {
